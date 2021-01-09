c3be3bd401a72700b6c47f1e3e0860a8c86373a0
Junior Hlongwane <juniorWebProjects@gmail.com>
2021-01-09 03:09:33 +0200

    Bug Fix: Broken links on mobile

--- client/src/Components/Home/MenuBar.jsx
+++ client/src/Components/Home/MenuBar.jsx
@@ -12,10 +12,12 @@ import { Link } from 'react-router-dom';
 export const MenuBar = () => {
   return (
     <div className="Menu">
-      <div className="Menu__cart">
-        <ShoppingCartIcon />
-        <h4 className="menu__link">Shopping Cart</h4>
-      </div>
+      <Link to="/cart" className="Router__link">
+        <div className="Menu__cart">
+          <ShoppingCartIcon />
+          <h4 className="menu__link">Shopping Cart</h4>
+        </div>
+      </Link>
       <div className="Menu__wishlist">
         <FavoriteIcon />
         <h4 className="menu__link">Wishlist</h4>
